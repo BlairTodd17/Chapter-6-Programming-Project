f97dde8f1a1c8185d309d46d60b37a9aa8436473
Blair Todd <bjbbt4@outlook.com>
2024-12-13 00:00:45 -0500

    q

--- Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
@@ -1,30 +1,70 @@
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 using namespace std;
 
-void displayTemperatureTable();
-double fahrenheitToCelsius(double fahrenheit);
+=void getScores(double scores[], int size);
+double calculateFinalScore(double scores[], int size);
+double findHighest(double scores[], int size);
+double findLowest(double scores[], int size);
+
+const int NUM_JUDGES = 5;
 
 int main() {
-    cout << "Fahrenheit to Celsius Temperature Table" << endl;
-    cout << "------------------------------------" << endl;
+    double scores[NUM_JUDGES];
 
-    displayTemperatureTable();
+    getScores(scores, NUM_JUDGES);
 
-    cout << "\nThank you for using the Celsius Temperature Table program!\n";
+    double finalScore = calculateFinalScore(scores, NUM_JUDGES);
+
+    cout << fixed << setprecision(2);
+    cout << "\nThe contestant's final score is: " << finalScore << endl;
+
     return 0;
 }
 
-void displayTemperatureTable() {
-    cout << setw(15) << "Fahrenheit" << setw(15) << "Celsius" << endl;
-    cout << "------------------------------------" << endl;
-
-    for (int fahrenheit = 0; fahrenheit <= 20; ++fahrenheit) {
-        double celsius = fahrenheitToCelsius(fahrenheit);
-        cout << setw(15) << fahrenheit << setw(15) << fixed << setprecision(2) << celsius << endl;
+void getScores(double scores[], int size) {
+    for (int i = 0; i < size; i++) {
+        do {
+            cout << "Enter score for judge " << (i + 1) << " (0-10): ";
+            cin >> scores[i];
+            if (scores[i] < 0.0 || scores[i] > 10.0) {
+                cout << "Invalid score. Scores must be between 0 and 10. Please try again.\n";
+            }
+        } while (scores[i] < 0.0 || scores[i] > 10.0);
     }
 }
 
-double fahrenheitToCelsius(double fahrenheit) {
-    return (5.0 / 9.0) * (fahrenheit - 32);
+double calculateFinalScore(double scores[], int size) {
+    double highest = findHighest(scores, size);
+    double lowest = findLowest(scores, size);
+    double sum = 0.0;
+
+    for (int i = 0; i < size; i++) {
+        sum += scores[i];
+    }
+
+    sum -= (highest + lowest);
+
+    return sum / (size - 2);
+}
+
+double findHighest(double scores[], int size) {
+    double highest = scores[0];
+    for (int i = 1; i < size; i++) {
+        if (scores[i] > highest) {
+            highest = scores[i];
+        }
+    }
+    return highest;
+}
+
+double findLowest(double scores[], int size) {
+    double lowest = scores[0];
+    for (int i = 1; i < size; i++) {
+        if (scores[i] < lowest) {
+            lowest = scores[i];
+        }
+    }
+    return lowest;
 }
